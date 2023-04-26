304058bcabbcc317ea3002c3869c442d65cee417
danhlucifer <danhit3003@gmail.com>
2023-04-26 11:31:30 +0700

    setup project & loginPage

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./App.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LoginPage } from './Routes'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
